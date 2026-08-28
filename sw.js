9de676eb11ab99137a71f2113273c54f78f8daa6
amramramr44000-cmyk <amramramr44000@gmail.com>
2026-08-28 16:07:47 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE = "rivo-shell-v5";
+const CACHE = "rivo-shell-v11";
 const ASSETS = [
   "./", "./index.html", "./css/style.css", "./js/core.js", "./js/app.js", "./js/supabase-config.js",
-  "./pages/login.html", "./pages/signup.html", "./pages/profile.html", "./pages/explore.html", "./pages/friends.html", "./pages/messages.html", "./pages/editor.html", "./pages/settings.html", "./pages/admin.html",
+  "./pages/login.html", "./pages/signup.html", "./pages/profile.html", "./pages/explore.html", "./pages/friends.html", "./pages/messages.html", "./pages/posts.html", "./pages/communities.html", "./pages/editor.html", "./pages/settings.html", "./pages/admin.html",
   "./manifest.webmanifest", "./assets/icon-192.png", "./assets/icon-512.png"
 ];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
